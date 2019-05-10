374290d770f00ae659f857ce375883b72b701053
ayrtonteshima <ayrtonteshima@gmail.com>
2019-05-10 17:16:48 -0300

    chore: WIP Setup inicial

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path:'/',
+        handler: (request, h) => {
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %ss', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
