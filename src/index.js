8a9f23df630bc50742d08b293fecbadc2858c606
ayrtonteshima <ayrtonteshima@gmail.com>
2019-06-05 20:33:45 -0300

    chore: adiciona eslint

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => 'Hello World!',
+  });
 
-    server.route({
-        method: 'GET',
-        path:'/',
-        handler: (request, h) => {
-            return 'Hello World!';
-        }
-    });
-
-    await server.start();
-    console.log('Server running on %ss', server.info.uri);
+  await server.start();
+  console.log('Server running on %ss', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
