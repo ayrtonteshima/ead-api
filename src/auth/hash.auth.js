e37cb9665bcb5d287fad3994995809a1faeb7743
ayrtonteshima <ayrtonteshima@gmail.com>
2019-08-25 08:44:26 -0300

    refactor: reduz logica da function make no hash de senha

--- src/auth/hash.auth.js
+++ src/auth/hash.auth.js
@@ -1,9 +1,8 @@
 const bcrypt = require('bcryptjs');
 
-const make = async (value) => {
-  const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(value, salt);
-};
+const make = async (value) => (
+  bcrypt.hash(value, 10)
+);
 
 const compare = (value, valueHash) => (
   bcrypt.compare(value, valueHash)
