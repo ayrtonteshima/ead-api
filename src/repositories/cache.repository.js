1605367d6ba1112d13e4ec9617e15b1b68400b78
Zaqueu C <zaqueudovale@gmail.com>
2020-08-29 16:05:27 -0300

    Separando código por contexto e extraindo métodos.

--- src/repositories/cache.repository.js
+++ src/repositories/cache.repository.js
@@ -1,5 +1,6 @@
 const Redis = require('../services/redis.service').get();
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 const set = (key, value, seconds) => (
   Redis.set(key, value, 'EX', seconds)
 );
@@ -8,6 +9,7 @@ const exists = key => Redis.exists(key);
 
 const del = key => Redis.del(key);
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 module.exports = {
   set,
   exists,
