d1ddec7ae51256e02ebc3b2a1d7e890147cd8f43
Zaqueu C <zaqueudovale@gmail.com>
2020-08-29 16:43:22 -0300

    Adicionando configurações de quebra de linha.

--- src/server.js
+++ src/server.js
@@ -6,36 +6,33 @@ const jwtStrategy = require('./auth/strategies/JWT');
 const { PORT } = process.env;
 const { HOST } = process.env;
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 const server = Hapi.server({
   port: PORT,
   host: HOST,
 });
 
-// Definindo rotas
 server.route(routes);
-
-const initializePlugins = async () => {
-  await server.register(hapiAuthJwt2);
-
-  // Definindo estratégia de autenticação
-  server.auth.strategy(jwtStrategy.name, jwtStrategy.schema, jwtStrategy.options);
-  server.auth.default(jwtStrategy.name);
-};
 
 const start = async () => {
   await initializePlugins();
   await server.start();
-
   return server;
 };
 
 const init = async () => {
   await initializePlugins();
   await server.initialize();
-
   return server;
 };
 
+const initializePlugins = async () => {
+  await server.register(hapiAuthJwt2);
+  server.auth.strategy(jwtStrategy.name, jwtStrategy.schema, jwtStrategy.options);
+  server.auth.default(jwtStrategy.name);
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 module.exports = {
   start,
   init,
