28a1facdfee06ebdf9261f73e25ce19539117b5d
Zaqueu C <zaqueudovale@gmail.com>
2020-08-29 15:42:24 -0300

    Organizando campos do payload.

--- src/schemas/users.schema.js
+++ src/schemas/users.schema.js
@@ -2,18 +2,18 @@ const Joi = require('@hapi/joi');
 
 module.exports = Joi.object({
   name: Joi.string().min(3).required(),
-  dateOfBirth: Joi.date().iso(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  birthDate: Joi.date().iso(),
   docType: Joi.string(),
   docNumber: Joi.string().min(3),
-  email: Joi.string().email().required(),
   status: Joi.string(),
-  password: Joi.string().min(6).required(),
   address: {
-    street: Joi.string(),
     country: Joi.string(),
     state: Joi.string(),
     city: Joi.string(),
-    zipcode: Joi.string(),
+    zipCode: Joi.string(),
+    street: Joi.string(),
     number: Joi.string(),
     complement: Joi.string(),
   },
